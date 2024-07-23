cd63a43a2a37446f20a95f115d048e428ec2694b
Tom(한재섭) <serve1103@jinhakapply.com>
2024-07-23 17:21:04 +0900

    :heavy_plus_sign: app 모듈 경로 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConcertModule } from './model/concert.module';
-import { AuthModule } from './model/auth.module';
-import { AmountModule } from './model/amount.module';
-import { PaymentModule } from './model/payment.module';
-import { UserInfoModule } from './model/userInfo.module';
-import { DatabaseModule } from './model/infrastructure/database.module';
+import { ConcertModule } from '@app/concert.module';
+import { AuthModule } from '@app/auth.module';
+import { AmountModule } from '@app/amount.module';
+import { PaymentModule } from '@app/payment.module';
+import { UserInfoModule } from '@app/userInfo.module';
+import { DatabaseModule } from '@app/infrastructure/database.module';
 
 @Module({
   imports: [
