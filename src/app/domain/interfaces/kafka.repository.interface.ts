a87d423addd699acce99f5f5701ab017e592b4c3
Tom(한재섭) <serve1103@jinhakapply.com>
2024-08-16 17:05:38 +0900

    ⚡️ kafka 기능 작업

--- src/app/domain/interfaces/kafka.repository.interface.ts
+++ src/app/domain/interfaces/kafka.repository.interface.ts
@@ -1,6 +1,8 @@
 import { KafkaOutboxModel, MessageState } from '@app/domain/models/kafka.model';
 
+export const KafkaRepositorySymbol = Symbol.for('KafkaRepository');
+
 export interface KafkaRepository {
-  findByState(status: MessageState): Promise<KafkaOutboxModel>;
+  findByState(status: MessageState): Promise<KafkaOutboxModel[]>;
   save(outbox: KafkaOutboxModel): Promise<boolean>;
 }
--- src/app/infrastructure/kafka/kafka.oubox.writer.ts
+++ src/app/infrastructure/kafka/kafka.oubox.writer.ts
@@ -1,14 +1,22 @@
 import { KafkaRepository } from '@app/domain/interfaces/kafka.repository.interface';
 import { KafkaOutboxModel, MessageState } from '@app/domain/models/kafka.model';
-import { Injectable } from '@nestjs/common';
-import { DataSourceOptions } from 'typeorm';
+import { Inject, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSourceOptions, Repository } from 'typeorm';
+import { KafkaOutbox } from '@app/infrastructure/kafka/kafka.outbox.entity';
 
 @Injectable()
 export class OutboxWriter implements KafkaRepository {
-  async findByState(status: MessageState): Promise<KafkaOutboxModel> {
-    return;
+  constructor(
+    @InjectRepository(KafkaOutbox)
+    private readonly kafkaOutboxRepository: Repository<KafkaOutbox>,
+  ) {}
+  async findByState(status: MessageState): Promise<KafkaOutboxModel[]> {
+    return await this.kafkaOutboxRepository.find({
+      where: { status: MessageState.INIT },
+    });
   }
   async save(outbox: KafkaOutboxModel): Promise<boolean> {
-    return;
+    return await this.kafkaOutboxRepository.save(outbox);
   }
 }
